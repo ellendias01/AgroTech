395ae4438d798ab439f0856213cb8e034a737537
habbiner <habbiner.andrade@gmail.com>
2025-05-21 20:08:29 -0300

    Criação do filtro por dias

--- backend/routes/dataRoutes.js
+++ backend/routes/dataRoutes.js
@@ -5,7 +5,15 @@ const DadosSensor = require('../models/DadosSensor');
 
 router.get('/dados', async (req, res) => {
   try {
-    const dados = await DadosSensor.find();
+    const dias = parseInt(req.query.dias) || 7;
+
+    const dataLimite = new Date();
+    dataLimite.setDate(dataLimite.getDate() - dias);
+
+    const dados = await DadosSensor.find({
+      datetime: { $gte: dataLimite }
+    }).sort({ datetime: -1 }); // mais recentes primeiro
+
     res.json(dados);
   } catch (err) {
     res.status(500).json({ error: 'Erro ao buscar dados do sensor' });
