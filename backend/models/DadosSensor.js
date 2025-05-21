298c68c1f4ec151a87267d6e1653fcbf0409575b
habbiner <habbiner.andrade@gmail.com>
2025-05-21 19:12:18 -0300

    Correção banco de dados e arquivos

---
+++ backend/models/DadosSensor.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const dadosSensorSchema = new mongoose.Schema({
+  datetime: Date,
+  humidity: Number,
+  temperature: Number
+}, { collection: 'dados_sensor' });
+
+module.exports = mongoose.model('DadosSensor', dadosSensorSchema);
